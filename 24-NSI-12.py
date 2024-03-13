20f836522212a79e27573238f45012c6a30123da
redsthan <rousseaunathan84@gmail.com>
2024-03-13 11:55:28 +0100

    Update 24-NSI-12.py

--- 24-NSI-12.py
+++ 24-NSI-12.py
@@ -1,20 +1,34 @@
 from random import randint
 
-def plus_ou_moins():
-    nb_mystere = randint(1, ...) 
-    nb_test = int(input("Proposez un nombre entre 1 et 99 : "))
-    compteur = ... 
+def tri_selection(tab):
+    for i in range(len(tab)):
+        min = i
+        for j in range(i+1, len(tab)):
+            if tab[min] > tab[j]:
+                min = j
+        tab[i], tab[min] = tab[min], tab[i]
+    return tab
 
-    while nb_mystere != ... and compteur < ...: 
+tab = [1, 52, 6, -9, 12]
+tri_selection(tab)
+assert tab == [-9, 1, 6, 12, 52]
+
+def plus_ou_moins():
+    nb_mystere = randint(1, 99) 
+    nb_test = int(input("Proposez un nombre entre 1 et 99 : "))
+    compteur = 1
+
+    while nb_mystere != nb_test and compteur < 10: 
         compteur = compteur + 1
-        if nb_mystere ... nb_test: 
+        if nb_mystere > nb_test: 
             nb_test = int(input("Trop petit ! Testez encore : "))
         else:
             nb_test = int(input("Trop grand ! Testez encore : "))
 
     if nb_mystere == nb_test:
-        print ("Bravo ! Le nombre était ", ...) 
-        print("Nombre d'essais: ", ...) 
+        print ("Bravo ! Le nombre était ", nb_mystere) 
+        print("Nombre d'essais: ", compteur) 
     else:
-        print ("Perdu ! Le nombre était ", ...) 
+        print ("Perdu ! Le nombre était ", nb_mystere) 
 
+plus_ou_moins()
