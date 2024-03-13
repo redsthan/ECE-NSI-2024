f3d196a8a70bc196eca5af03952149fc5c5d72d2
redsthan <rousseaunathan84@gmail.com>
2024-03-13 11:49:09 +0100

    Update 24-NSI-11.py

--- 24-NSI-11.py
+++ 24-NSI-11.py
@@ -1,3 +1,16 @@
+def nombre_de_mots(phrase):
+    if phrase[-1] == '.':
+        return phrase.count(' ') + 1
+    return phrase.count(' ')
+
+
+assert nombre_de_mots('Cet exercice est simple.') == 4
+assert nombre_de_mots('Le point d exclamation est séparé !') == 6
+assert nombre_de_mots('Combien de mots y a t il dans cette phrase ?') == 10
+assert nombre_de_mots('Fin.') == 1
+
+
+
 class Noeud:
     def __init__(self, etiquette):
         '''Méthode constructeur pour la classe Noeud.
@@ -11,13 +24,21 @@ class Noeud:
         en préservant sa structure.'''
         if cle < self.etiquette:
             if self.gauche != None:
-                ...
+                self.gauche.inserer(cle)
             else:
-                self.gauche = ... 
+                self.gauche = Noeud(cle)
         else:
-            ...
-                ...
+            if self.droit != None:
+                self.droit.inserer(cle)
             else:
-                ... = Noeud(cle) 
+                self.droit = Noeud(cle) 
 
 
+arbre = Noeud(7)
+for cle in [3, 9, 1, 6]:
+    arbre.inserer(cle)
+assert arbre.etiquette == 7
+assert arbre.gauche.etiquette == 3
+assert arbre.droit.etiquette == 9
+assert arbre.gauche.gauche.etiquette == 1
+assert arbre.gauche.droit.etiquette == 6
