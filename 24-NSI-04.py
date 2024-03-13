32d6a11bfe2444fb0a733296bb02c5f3629e885d
redsthan <rousseaunathan84@gmail.com>
2024-03-13 10:20:36 +0100

    Update 24-NSI-04.py

--- 24-NSI-04.py
+++ 24-NSI-04.py
@@ -1,17 +1,31 @@
+def recherche(tab, n):
+    if n in tab:
+        return len(tab) - tab[::-1].index(n) -1
+    
+assert recherche([5, 3],1) is None
+assert recherche([2,4],2) == 0
+assert recherche([2,3,5,2,4],2) == 3
+
 def distance_carre(point1, point2):
     """ Calcule et renvoie la distance au carre entre 
     deux points."""
-    return (...)**2 + (...)**2 
+    return (point1[0]-point2[0])**2 + (point1[1]-point2[1])**2 
 
 def point_le_plus_proche(depart, tab):
     """ Renvoie les coordonnées du premier point du tableau tab se 
     trouvant à la plus courte distance du point depart."""
     min_point = tab[0]
-    min_dist = ... 
+    min_dist = distance_carre(tab[0], depart)
     for i in range(1, len(tab)):
-        if distance_carre(tab[i], depart) < ...: 
-            min_point = ... 
-            min_dist = ... 
+        if distance_carre(tab[i], depart) < min_dist: 
+            min_point = tab[i] 
+            min_dist = distance_carre(tab[i], depart)
     return min_point
+
+assert distance_carre((1, 0), (5, 3)) == 25
+assert distance_carre((1, 0), (0, 1)) == 2
+assert point_le_plus_proche((0, 0), [(7, 9), (2, 5), (5, 2)]) == (2, 5)
+assert point_le_plus_proche((5, 2), [(7, 9), (2, 5), (5, 2)]) == (5, 2)
+
 
 
