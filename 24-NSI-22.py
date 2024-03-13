2b3df14ef633025b1b2f98efbaf746d9b70f7ec9
redsthan <rousseaunathan84@gmail.com>
2024-03-13 19:45:23 +0100

    Update 24-NSI-22.py

--- 24-NSI-22.py
+++ 24-NSI-22.py
@@ -15,22 +15,42 @@ resultats = {
     }
 }
 
+def recherche_indices_classement(elt, tab):
+    si = []
+    eq = []
+    ss = []
+    for i, e in enumerate(tab):
+        if e < elt:
+            si.append(i)
+        elif e == elt:
+            eq.append(i)
+        else:
+            ss.append(i)
+    return si, eq, ss
+
+assert recherche_indices_classement(3, [1, 3, 4, 2, 4, 6, 3, 0]) == ([0, 3, 7], [1, 6], [2, 4, 5])
+assert recherche_indices_classement(3, [1, 4, 2, 4, 6, 0]) == ([0, 2, 5], [], [1, 3, 4])
+assert recherche_indices_classement(3, [1, 1, 1, 1]) == ([0, 1, 2, 3], [], [])
+assert recherche_indices_classement(3, []) == ([], [], [])
+
+
 def moyenne(nom, resultats):
     '''Renvoie la moyenne de l'élève nom, selon le dictionnaire 
     resultats. Si nom n'est pas dans le dictionnaire, 
     la fonction renvoie None.'''
-    if nom in ...: 
+    if nom in resultats: 
         notes = resultats[nom]
-        if ...: # pas de notes 
+        if not notes: # pas de notes 
             return 0
-        total_points = ... 
-        total_coefficients = ... 
-        for ...  in notes.values(): 
+        total_points = 0
+        total_coefficients = 0 
+        for valeurs in notes.values(): 
             note, coefficient = valeurs
-            total_points = total_points + ... * coefficient 
-            ... = ... + coefficient 
-        return round( ... / total_coefficients, 1 ) 
+            total_points = total_points + note * coefficient 
+            total_coefficients = total_coefficients + coefficient 
+        return round( total_points / total_coefficients, 1 ) 
     else:
         return None
 
-
+assert moyenne("Dupont", resultats)==14.5
+assert moyenne("Durand", resultats)==8.5
