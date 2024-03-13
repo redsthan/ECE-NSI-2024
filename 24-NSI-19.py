13c805b8fa4c7dabd936f0cf1ab918abb58b6cd5
redsthan <rousseaunathan84@gmail.com>
2024-03-13 19:27:55 +0100

    Update 24-NSI-19.py

--- 24-NSI-19.py
+++ 24-NSI-19.py
@@ -4,6 +4,26 @@ dico = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6,
         "R": 18, "S": 19, "T": 20, "U": 21, "V": 22,
         "W": 23, "X": 24, "Y": 25, "Z": 26}
 
+
+def liste_puissances(a, n):
+    L = [a]
+    for i in range(1, n):
+        L.append(L[-1] * a)
+    return L
+
+def liste_puissances_borne(a, n):
+    L = [a]
+    while L[-1] < n:
+        L.append(L[-1] * a)
+    return L[:-1]
+
+assert liste_puissances(3, 5) == [3, 9, 27, 81, 243]
+assert liste_puissances(-2, 4) == [-2, 4, -8, 16]
+assert liste_puissances_borne(2, 16) == [2, 4, 8]
+assert liste_puissances_borne(2, 17) == [2, 4, 8, 16]
+assert liste_puissances_borne(5, 5) == []
+
+
 def codes_parfait(mot):
     """Renvoie un triplet 
     (code_additionne, code_concatene, mot_est_parfait) où :
@@ -11,12 +31,14 @@ def codes_parfait(mot):
     - code_concatene est le code des lettres du mot concaténées ;
     - mot_est_parfait est un booléen indiquant si le mot est parfait."""
     code_concatene = ""
-    code_additionne = ... 
+    code_additionne = 0
     for c in mot:
-        code_concatene = code_concatene + ... 
-        code_additionne = code_additionne + ... 
+        code_concatene = code_concatene + str(dico[c])
+        code_additionne = code_additionne + dico[c]
     code_concatene = int(code_concatene)
-    mot_est_parfait = ... 
+    mot_est_parfait = code_concatene % code_additionne == 0
     return code_additionne, code_concatene, mot_est_parfait
 
 
+assert codes_parfait("PAUL")==(50, 1612112, False)
+assert codes_parfait("ALAIN")==(37, 1121914, True)
