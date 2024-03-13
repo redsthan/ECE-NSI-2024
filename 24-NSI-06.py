f06e9bd8039770d11429a790f303de2851354a2d
redsthan <rousseaunathan84@gmail.com>
2024-03-13 10:42:25 +0100

    Update 24-NSI-06.py

--- 24-NSI-06.py
+++ 24-NSI-06.py
@@ -1,12 +1,23 @@
+def verifie(tab):
+    return tab == sorted(tab)
+
+assert verifie([0, 5, 8, 8, 9])
+assert not verifie([8, 12, 4])
+assert verifie([-1, 4])
+assert verifie([])
+assert verifie([5])
+
+
+
 def depouille(urne):
     '''prend en paramètre une liste de suffrages et renvoie un 
     dictionnaire avec le nombre de voix pour chaque candidat'''
-    resultat = ... 
+    resultat = {}
     for bulletin in urne:
-        if ...: 
+        if bulletin in resultat: 
             resultat[bulletin] = resultat[bulletin] + 1
         else:
-            ...
+            resultat[bulletin] = 1
     return resultat
 
 def vainqueurs(election):
@@ -14,9 +25,15 @@ def vainqueurs(election):
     pour chaque candidat et renvoie la liste des vainqueurs'''
     nmax = 0
     for candidat in election:
-        if ... > ... : 
-            nmax = ... 
-    liste_finale = [ nom for nom in election if ... ] 
-    return ... 
+        if election[candidat] > nmax : 
+            nmax = election[candidat]
+    liste_finale = [ nom for nom in election if election[nom] == nmax] 
+    return liste_finale
 
 
+assert depouille([ 'A', 'B', 'A' ]) == {'A': 2, 'B': 1}
+assert depouille([]) == {}
+election = depouille(['A', 'A', 'A', 'B', 'C', 'B', 'C', 'B', 'C', 'B'])
+assert election == {'A': 3, 'B': 4, 'C': 3}
+assert vainqueurs(election) == ['B']
+assert vainqueurs({ 'A' : 2, 'B' : 2, 'C' : 1}) == ['A', 'B']
