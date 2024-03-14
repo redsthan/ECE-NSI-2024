9361aa54a34b577aef471278e82c575abd7d613a
redsthan <rousseaunathan84@gmail.com>
2024-03-14 07:29:56 +0100

    Update 24-NSI-24.py

--- 24-NSI-24.py
+++ 24-NSI-24.py
@@ -1,19 +1,40 @@
+from collections import deque
+
+def parcours_largeur(abr):
+    file = deque([abr])
+    lst = []
+    while file:
+        current = file.popleft()
+        if current is not None:
+            lst.append(current[1])
+            file.append(current[0])
+            file.append(current[2])
+    return lst
+            
+arbre = ( ( (None, 1, None), 2, (None, 3, None) ),
+4,
+( (None, 5, None), 6, (None, 7, None) ) )
+assert parcours_largeur(arbre)==[4, 2, 6, 1, 3, 5, 7]
+
 def somme_max(tab):
     n = len(tab)
     sommes_max = [0]*n
     sommes_max[0] = tab[0]
     # on calcule la plus grande somme se terminant en i
     for i in range(1,n):
-        if ... + ... > ...: 
-            sommes_max[i] = ... 
+        if sommes_max[i-1] + tab[i] > tab[i]: 
+            sommes_max[i] = sommes_max[i-1] + tab[i]
         else:
-            sommes_max[i] = ... 
+            sommes_max[i] = tab[i]
     # on en déduit la plus grande somme de celles-ci
     maximum = 0
     for i in range(1, n):
-        if ... > ...: 
+        if sommes_max[i] > sommes_max[maximum]: 
             maximum = i
 
-    return sommes_max[...] 
+    return sommes_max[maximum] 
 
-
+assert somme_max([1, 2, 3, 4, 5]) == 15
+assert somme_max([1, 2, -3, 4, 5]) == 9
+assert somme_max([1, 2, -2, 4, 5]) == 10
+assert somme_max([1, -2, 3, 10, -4, 7, 2, -5]) == 18
