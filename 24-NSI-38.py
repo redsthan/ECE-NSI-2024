d7b2787a6d5bb5dfd8ebd2a1991aaaef9b89b4ca
redsthan <rousseaunathan84@gmail.com>
2024-03-14 09:04:57 +0100

    Update 24-NSI-38.py

--- 24-NSI-38.py
+++ 24-NSI-38.py
@@ -1,21 +1,39 @@
+def indices_maxi(tab):
+    assert tab != []
+    maxi = tab[0]
+    indices = [0]
+    for i in range(1, len(tab)):
+        if tab[i] > maxi:
+            maxi = tab[i]
+            indices = [i]
+        elif tab[i] == maxi:
+            indices.append(i)
+    return maxi, indices
+
+assert indices_maxi([1, 5, 6, 9, 1, 2, 3, 7, 9, 8])==(9, [3, 8])
+assert indices_maxi([7])==(7, [0])
+
 def renverse(pile):
     '''renvoie une pile contenant les mêmes éléments que pile,
     mais dans l'ordre inverse.
     Cette fonction détruit pile.'''
-    pile_inverse = ... 
+    pile_inverse = []
     while pile != []:
-        ... .append(...) 
-    return ... 
+        pile_inverse.append(pile.pop()) 
+    return pile_inverse
 
 
 def positifs(pile):
     '''renvoie une pile contenant les éléments positifs de pile,
     dans le même ordre. Cette fonction détruit pile.'''
-    pile_positifs = ... 
+    pile_positifs = []
     while pile != []:
-        ... = pile.pop() 
-        if ... >= 0: 
-            ...
-    return ... 
+        elem = pile.pop() 
+        if elem >= 0: 
+            pile_positifs.append(elem)
+    return renverse(pile_positifs) 
 
 
+assert renverse([1, 2, 3, 4, 5])==[5, 4, 3, 2, 1]
+assert positifs([-1, 0, 5, -3, 4, -6, 10, 9, -8])==[0, 5, 4, 10, 9]
+assert positifs([-2])==[]
