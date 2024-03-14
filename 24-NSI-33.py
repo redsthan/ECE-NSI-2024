b08ae143925ab70aaf4a20f0346e0acbbd21cc78
redsthan <rousseaunathan84@gmail.com>
2024-03-14 08:35:11 +0100

    Update 24-NSI-33.py

--- 24-NSI-33.py
+++ 24-NSI-33.py
@@ -1,3 +1,10 @@
+def renverse(chr):
+    return chr[::-1]
+
+assert renverse("") == ""
+assert renverse("abc") == "cba"
+assert renverse("informatique") == "euqitamrofni"
+
 def crible(n):
     """Renvoie un tableau contenant tous les nombres premiers
     plus petits que n."""
@@ -6,11 +13,14 @@ def crible(n):
     tab[0], tab[1] = False, False
     for i in range(n):
         if tab[i]:
-            premiers.... 
-            multiple = ... 
+            premiers.append(i)
+            multiple = i * 2
             while multiple < n:
-                tab[multiple] = ... 
-                multiple = ... 
+                tab[multiple] = False
+                multiple = multiple + i
     return premiers
+
+assert crible(40) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+assert crible(5) == [2, 3]
 
 
