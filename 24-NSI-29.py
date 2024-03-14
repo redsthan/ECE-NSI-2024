06b903358505ef5f6f81ef4f9a60fa7e53215724
redsthan <rousseaunathan84@gmail.com>
2024-03-14 08:14:07 +0100

    Update 24-NSI-29.py

--- 24-NSI-29.py
+++ 24-NSI-29.py
@@ -1,17 +1,26 @@
+def moyenne(notes):
+    num = sum([n*c for n, c in notes])
+    den = sum([c for n, c in notes])
+    return num / den
+
+assert moyenne([(15.0, 2), (9.0, 1), (12.0, 3)]) == 12.5
+
 def ligne_suivante(ligne):
     '''Renvoie la ligne suivant ligne du triangle de Pascal'''
-    ligne_suiv = [...] 
-    for i in range(...): 
-        ligne_suiv.append(...) 
-    ligne_suiv.append(...) 
+    ligne_suiv = [1] 
+    for i in range(1, len(ligne)): 
+        ligne_suiv.append(ligne[i-1] + ligne[i]) 
+    ligne_suiv.append(1) 
     return ligne_suiv
 
 def pascal(n):
     '''Renvoie le triangle de Pascal de hauteur n'''
     triangle = [ [1] ]
-    for k in range(...): 
-        ligne_k = ... 
+    for k in range(n): 
+        ligne_k = ligne_suivante(triangle[k])
         triangle.append(ligne_k)
     return triangle
 
-
+assert ligne_suivante([1, 3, 3, 1]) == [1, 4, 6, 4, 1]
+assert pascal(2) == [[1], [1, 1], [1, 2, 1]]
+assert pascal(3) == [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]]
