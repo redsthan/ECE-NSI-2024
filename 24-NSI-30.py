8e070908495da089289094eec2074144d97a9ad1
redsthan <rousseaunathan84@gmail.com>
2024-03-14 08:18:49 +0100

    Update 24-NSI-30.py

--- 24-NSI-30.py
+++ 24-NSI-30.py
@@ -1,11 +1,27 @@
+from heapq import merge
+
+def fusion(*iter):
+    return list(merge(*iter))
+
+assert fusion([3, 5], [2, 5]) == [2, 3, 5, 5]
+assert fusion([-2, 4], [-3, 5, 10]) == [-3, -2, 4, 5, 10]
+assert fusion([4], [2, 6]) == [2, 4, 6]
+assert fusion([], []) == []
+assert fusion([1, 2, 3], []) == [1, 2, 3]
+
+romains = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500,"M":1000}
+
 def traduire_romain(nombre):
     """ Renvoie l'écriture décimale du nombre donné en chiffres
     romains """
     if len(nombre) == 1:
-        return ... 
-    elif romains[nombre[0]] >= ...: 
-        return romains[nombre[0]] + ... 
+        return romains[nombre] 
+    elif romains[nombre[0]] >= romains[nombre[1]]: 
+        return romains[nombre[0]] + traduire_romain(nombre[1:])
     else:
-        return ... 
+        return -romains[nombre[0]] + traduire_romain(nombre[1:])
 
 
+assert traduire_romain("XIV") == 14
+assert traduire_romain("CXLII") == 142
+assert traduire_romain("MMXXIV") == 2024
