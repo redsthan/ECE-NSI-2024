06cc10440019855c43250d2822bf7fd0be7e3417
redsthan <rousseaunathan84@gmail.com>
2024-03-14 08:30:52 +0100

    Update 24-NSI-32.py

--- 24-NSI-32.py
+++ 24-NSI-32.py
@@ -1,3 +1,14 @@
+def ou_exclusif(tab1, tab2):
+    '''tab1 et tab2 sont des listes de même longueur'''
+    assert len(tab1) == len(tab2), "Les deux listes doivent être de même longueur"
+    res = []
+    for i in range(len(tab1)):
+        res.append(tab1[i] ^ tab2[i])
+    return res
+
+assert ou_exclusif([1, 0, 1, 0, 1, 1, 0, 1], [0, 1, 1, 1, 0, 1, 0, 0]) == [1, 1, 0, 1, 1, 0, 0, 1]
+assert ou_exclusif([1, 1, 0, 1], [0, 0, 1, 1]) == [1, 1, 1, 0]
+
 class Carre:
     def __init__(self, liste, n): 
         self.ordre = n        
@@ -29,15 +40,25 @@ class Carre:
     def est_semimagique(self): 
         s = self.somme_ligne(0) 
         #test de la somme de chaque ligne 
-        for i in range(...): 
-            if ... != s: 
-                return ... 
+        for i in range(1, self.ordre): 
+            if self.somme_ligne(i) != s: 
+                return False
                                    
         #test de la somme de chaque colonne 
-        for j in range(...): 
-            if ... != s: 
-                return ... 
+        for j in range(self.ordre): 
+            if self.somme_col(j) != s: 
+                return False
                                    
-        return ... 
+        return True
+    
+lst_c2 = [1, 7, 7, 1]
+c2 = Carre(lst_c2, 2)
+lst_c3 = [3, 4, 5, 4, 4, 4, 5, 4, 3]
+c3 = Carre(lst_c3, 3)
+lst_c3bis = [2, 9, 4, 7, 0, 3, 6, 1, 8]
+c3bis = Carre(lst_c3bis, 3)
+assert c2.est_semimagique()
+assert c3.est_semimagique()
+assert not c3bis.est_semimagique()
 
 
