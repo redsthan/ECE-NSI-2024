0e4e6a81af2943766954c00e7374c6d2b10272fe
redsthan <rousseaunathan84@gmail.com>
2024-03-14 08:24:04 +0100

    Update 24-NSI-31.py

--- 24-NSI-31.py
+++ 24-NSI-31.py
@@ -1,3 +1,21 @@
+def multiplication(a, b):
+    """
+    a et b : deux entiers
+    La fonction renvoie le produit de a par b
+    """
+    if b == 0:
+        return 0
+    if b > 0:
+        return a + multiplication(a, b - 1)
+    if b < 0:
+        return -multiplication(a, -b)
+
+assert multiplication(3, 5) == 15
+assert multiplication(-4, -8) == 32
+assert multiplication(-2, 6) == -12
+assert multiplication(-2, 0) == 0    
+
+
 def dichotomie(tab, x):
     """
     tab : tableau d'entiers trié dans l'ordre croissant
@@ -7,13 +25,15 @@ def dichotomie(tab, x):
     debut = 0
     fin = len(tab) - 1
     while debut <= fin:
-        m = ... 
+        m = (debut + fin) // 2
         if x == tab[m]:
-            return ... 
+            return True
         if x > tab[m]:
             debut = m + 1
         else:
-            fin = ... 
-    return ... 
+            fin = m-1
+    return False
 
 
+assert dichotomie([15, 16, 18, 19, 23, 24, 28, 29, 31, 33],28)
+assert not dichotomie([15, 16, 18, 19, 23, 24, 28, 29, 31, 33],27)
