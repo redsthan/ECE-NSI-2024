10c8a76741eaa32065691a0789fece9e3984a1d9
redsthan <rousseaunathan84@gmail.com>
2024-03-14 08:04:54 +0100

    Update 24-NSI-27.py

--- 24-NSI-27.py
+++ 24-NSI-27.py
@@ -1,3 +1,18 @@
+def couples_consecutifs(tab):
+    couples = []
+    prec = tab[0]
+    for elem in tab[1:]:
+        if elem == prec+1:
+            couples.append((prec, elem))
+        prec = elem
+    return couples
+
+assert couples_consecutifs([1, 4, 3, 5]) == []
+assert couples_consecutifs([1, 4, 5, 3]) == [(4, 5)]
+assert couples_consecutifs([1, 1, 2, 4]) == [(1, 2)]
+assert couples_consecutifs([7, 1, 2, 5, 3, 4]) == [(1, 2), (3, 4)]
+assert couples_consecutifs([5, 1, 2, 3, 8, -5, -4, 7]) == [(1, 2), (2, 3), (-5, -4)]
+
 def colore_comp1(M, i, j, val):
     if M[i][j] != 1:
         return
@@ -6,11 +21,15 @@ def colore_comp1(M, i, j, val):
 
     if i-1 >= 0: # propage à gauche
         colore_comp1(M, i-1, j, val)
-    if ... < len(M): # propage à droite 
-        colore_comp1(M, ..., j, val) 
-    if ...: # propage en haut 
-        colore_comp1(M, ..., ..., val) 
-    if ...: # propage en bas 
-        ...
+    if i+1 < len(M): # propage à droite 
+        colore_comp1(M, i+1, j, val) 
+    if j-1 < len(M[0]): # propage en haut 
+        colore_comp1(M, i, j-1, val) 
+    if j+1 < len(M[0]): # propage en bas 
+        colore_comp1(M, i, j+1, val)
+        
+        
 
-
+M = [[0, 0, 1, 0], [0, 1, 0, 1], [1, 1, 1, 0], [0, 1, 1, 0]]
+colore_comp1(M, 2, 1, 3)
+assert M==[[0, 0, 1, 0], [0, 3, 0, 1], [3, 3, 3, 0], [0, 3, 3, 0]]
