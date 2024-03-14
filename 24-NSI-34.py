d0273a599919cacdc42eb68b4fb7fc369f392e71
redsthan <rousseaunathan84@gmail.com>
2024-03-14 08:38:46 +0100

    Update 24-NSI-34.py

--- 24-NSI-34.py
+++ 24-NSI-34.py
@@ -1,3 +1,10 @@
+from collections import Counter
+
+def nbr_occurrences(chr):
+    return dict(Counter(chr))
+
+assert nbr_occurrences("Hello world !") == {'H': 1, 'e': 1, 'l': 3, 'o': 2, ' ': 2, 'w': 1, 'r': 1, 'd': 1, '!': 1}
+
 def fusion(tab1,tab2):
     '''Fusionne deux tableaux triés et renvoie
     le nouveau tableau trié.'''
@@ -7,22 +14,26 @@ def fusion(tab1,tab2):
     i1 = 0
     i2 = 0
     i = 0
-    while i1 < n1 and ...: 
+    while i1 < n1 and i2 < n2: 
         if tab1[i1] < tab2[i2]:
-            tab12[i] = ... 
-            i1 = ... 
+            tab12[i] = tab1[i1]
+            i1 = i1 + 1
         else:
             tab12[i] = tab2[i2]
-            i2 = ... 
+            i2 = i2 + 1
         i += 1
     while i1 < n1:
-        tab12[i] = ... 
+        tab12[i] = tab1[i1] 
         i1 = i1 + 1
-        i = ... 
+        i = i + 1
     while i2 < n2:
-        tab12[i] = ... 
+        tab12[i] = tab2[i2]
         i2 = i2 + 1
-        i = ... 
+        i = i + 1
     return tab12
+
+assert fusion([1,2,3],[]) == [1, 2, 3]
+assert fusion([], []) == []
+assert fusion([1, 6, 10],[0, 7, 8, 9]) == [0, 1, 6, 7, 8, 9, 10]
 
 
